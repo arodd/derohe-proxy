4e0033016bad4b539633db94c5c15839a85a37d9
Austin Workman <soilflames@gmail.com>
2022-05-24 15:06:26 -0500

    Adding more randomness

--- proxy/feature.go
+++ proxy/feature.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"math/rand"
+	"crypto/rand"
 
 	"github.com/deroproject/derohe/block"
 	"github.com/deroproject/derohe/rpc"
@@ -17,6 +17,7 @@ func edit_blob(input []byte) (output []byte) {
 	var mbl block.MiniBlock
 	var raw_hex []byte
 	var out bytes.Buffer
+	var val int
 
 	if err = json.Unmarshal(input, &params); err != nil {
 		return
@@ -29,11 +30,20 @@ func edit_blob(input []byte) (output []byte) {
 	if mbl.Deserialize(raw_hex); err != nil {
 		return
 	}
+	key := [4]byte{}
 
 	for i := range mbl.Nonce {
-		mbl.Nonce[i] = rand.Uint32()
+		val, err = rand.Read(key[:])
+		if err != nil {
+		    panic(err)
+		}
+		mbl.Nonce[i] = uint32(val)
 	}
-	mbl.Flags = rand.Uint32()
+	val, err = rand.Read(key[:])
+	if err != nil {
+            panic(err)
+        }
+	mbl.Flags = uint32(val)
 
 	params.Blockhashing_blob = fmt.Sprintf("%x", mbl.Serialize())
 	encoder := json.NewEncoder(&out)
