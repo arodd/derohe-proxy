9ca607c187f632c087c28d01981a51bef93d69b7
Austin Workman <austin@hashicorp.com>
2022-06-15 14:22:14 -0500

    Adding output to show broken nonce/flags generation

--- proxy/feature.go
+++ proxy/feature.go
@@ -2,14 +2,14 @@ package proxy
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"crypto/rand"
 
 	"github.com/deroproject/derohe/block"
-	"github.com/deroproject/derohe/rpc"
 	"github.com/deroproject/derohe/globals"
+	"github.com/deroproject/derohe/rpc"
 )
 
 func edit_blob(input []byte) (output []byte) {
@@ -36,17 +36,17 @@ func edit_blob(input []byte) (output []byte) {
 	for i := range mbl.Nonce {
 		val, err = rand.Read(key[:])
 		if err != nil {
-		    panic(err)
+			panic(err)
 		}
 		mbl.Nonce[i] = uint32(val)
 	}
 	val, err = rand.Read(key[:])
 	if err != nil {
-            panic(err)
-        }
+		panic(err)
+	}
 	mbl.Flags = uint32(val)
 	timestamp := uint64(globals.Time().UTC().UnixMilli())
-	mbl.Timestamp = uint16(timestamp) // this will help us better understand network conditions 
+	mbl.Timestamp = uint16(timestamp) // this will help us better understand network conditions
 
 	params.Blockhashing_blob = fmt.Sprintf("%x", mbl.Serialize())
 	encoder := json.NewEncoder(&out)
@@ -54,7 +54,7 @@ func edit_blob(input []byte) (output []byte) {
 	if err = encoder.Encode(params); err != nil {
 		return
 	}
-
+	fmt.Println("Nonces:", mbl.Nonce[0], mbl.Nonce[1], mbl.Nonce[2], "Flags:", mbl.Flags)
 	output = out.Bytes()
 
 	return
